b5b258a47e29e3ae0f8e95df1b77a8ebc9cada4c
Axelle <guinaudeau.ion1516@gmail.com>
2021-05-05 19:31:04 +0200

    add webservice api

--- app/src/main/java/com/example/tp_projet_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/tp_projet_kotlin/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.tp_projet_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tp_projet_kotlin.RetrofitSingleton.courseRequest
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,13 +24,32 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         liste_voitures.layoutManager = layoutManager
 
-        // contenu d'exemple :
+        // récupération des voitures
         val listeVoitures: MutableList<Voiture> = ArrayList()
-        listeVoitures.add(Voiture("Buggy", "19", "G"))
-        listeVoitures.add(Voiture("Damn Car", "12", "E"))
+        if (ReseauHelper.estConnecte(this)) {
+            courseRequest.enqueue(object : Callback<List<RetourWSGet>> {
+                override fun onResponse(
+                    call: Call<List<RetourWSGet>>,
+                    response: Response<List<RetourWSGet>>
+                ) {
+                    val allCar = response.body()
+                    if (allCar != null) {
+                        for (c in allCar) {
+                            listeVoitures.add(Voiture(c.nom, c.prixjournalierbase.toString(), c.categorieco2))
 
-        // adapter :
-        val voituresAdapter = VoituresAdapter(listeVoitures)
-        liste_voitures.adapter = voituresAdapter
+                            // adapter :
+                            val voituresAdapter = VoituresAdapter(listeVoitures)
+                            liste_voitures.adapter = voituresAdapter
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<List<RetourWSGet>>, t: Throwable) {
+                    error("KO")
+                    //prévoir message erreur
+                }
+            })
+        }
     }
+
 }
--- app/src/main/java/com/example/tp_projet_kotlin/RetourWsGet.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tp_projet_kotlin
-
-data class RetourWSGet(
-        val attribut1: String,
-        val attribut2: String,
-        val attribut3: String
-)
